5d9522070eb635883afab76f337bf4306794ec43
alireza793 <Mr.Alireza.Hakemi@proton.me>
2025-09-06 14:16:06 -0400

    Update transformers_example1.py

--- transformers_test.py
+++ transformers_test.py
@@ -1,6 +1,12 @@
 from transformers import pipeline
-
-classifier = pipeline("sentiment-analysis")
-
-print(classifier("I love learning AI with Python!"))
-print(classifier("This project is very hard and frustrating."))
+import pandas as pd
+classifier = pipeline("text-classification")
+text = """Dear Amazon, last week I ordered an Optimus Prime action figure
+from your online store in Germany. Unfortunately, when I opened the package,
+I discovered to my horror that I had been sent an action figure of Megatron
+instead! As a lifelong enemy of the Decepticons, I hope you can understand my
+dilemma. To resolve the issue, I demand an exchange of Megatron for the
+Optimus Prime figure I ordered. Enclosed are copies of my records concerning
+this purchase. I expect to hear from you soon. Sincerely, Bumblebee."""
+outputs = classifier(text)
+pd.DataFrame(outputs)
